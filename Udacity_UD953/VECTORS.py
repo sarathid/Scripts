e521041eebdab58d3238d355838809f7fe716997
Partha Saradhi <sarathi.d33@gmail.com>
2017-01-22 15:57:16 +0530

    Updated code for changes in Unit 1

--- Udacity_UD953/VECTORS.py
+++ Udacity_UD953/VECTORS.py
@@ -41,7 +41,27 @@ def angle(v,w, deg=False):
 		return r if deg is False else degrees((Decimal(r)))
 	except:
 		print 'Pass'
+
+def cross_prod(v,w):
+	x1 = Decimal(v[0])
+	y1 = Decimal(v[1])
+	z1 = Decimal(v[2])
+	x2 = Decimal(w[0])
+	y2 = Decimal(w[1])
+	z2 = Decimal(w[2])
+	result = []
+	result.append(y1*z2 - y2*z1)
+	result.append(-(x1*z2 - x2*z1))
+	result.append(x1*y2 - x2*y1)
+	return result
 	
+def area_of_parallogram(v,w):
+	vxw = cross_prod(v,w)
+	return vectorMag(vxw)
+
+def area_of_triangle(v,w):
+	return area_of_parallogram(v,w)/2
+
 # print angle([-7.579,-7.88],[22.737, 23.64])
 # print angle([3.183,-7.627],[-2.668, 5.319])
 # print angle([-7.579,-7.88],[22.737, 23.64])
@@ -76,16 +96,31 @@ def angle(v,w, deg=False):
 # print 'Vector parallel', vper
 
 
-v = [3.009, -6.172, 3.692, -2.51]
-b = [6.404, -9.144, 2.759, 8.718]
-ub = unit_vector(b)
-print ub
-dpub = dot_prod(v, ub)
+# v = [3.009, -6.172, 3.692, -2.51]
+# b = [6.404, -9.144, 2.759, 8.718]
+# ub = unit_vector(b)
+# print ub
+# dpub = dot_prod(v, ub)
 
-if angle(v,b, True)>=90:
-	dpub = -dpub
+# if angle(v,b, True)>=90:
+# 	dpub = -dpub
 
-vp = mulVector(ub, dpub)
-vper = vector_sub(v, vp)
-print 'Vector Parallel',vp 
-print 'Vector perp', vper
+# vp = mulVector(ub, dpub)
+# vper = vector_sub(v, vp)
+# print 'Vector Parallel',vp 
+# print 'Vector perp', vper
+
+
+v = [8.462, 7.893, -8.187]
+w = [6.984, -5.975, 4.778]
+
+print 'Cross-prod ',cross_prod(v,w)
+
+v = [-8.987, -9.838, 5.031]
+w = [-4.268, -1.861, -8.866]
+
+print 'Area of parall', area_of_parallogram(v,w)
+
+v = [1.5, 9.547, 3.691]
+w = [-6.007, 0.124, 5.772]
+print 'Area of Triangle', area_of_triangle(v,w)
