f853568ac482f42e1ad9cd6a4e4698307a511cea
Parthasaradhi <sarathi.d33@gmail.com>
2017-07-02 16:39:58 +0530

    Added filters

--- Script1.py
+++ Script1.py
@@ -1,16 +1,16 @@
 import os
 
-log = r"C:\Users\Admin\Desktop\Flog.txt"
+log = r"/home/parthasaradhi/Flog.txt"
 
 flog = open(log,'w')
-path = r'c:\\'
+path = r'/media/parthasaradhi/Seagate Backup Plus Drive'
 file_map = {}
 for folder,subfolder,files in os.walk(path):
 	for f in files:
 		fp = os.path.join(folder,f)
 		# print fp
 		# flog.write(fp+'\n')
-		if os.path.isfile(fp):
+		if os.path.isfile(fp) and os.path.getsize(fp)>2097152:
 			file_map[fp] = {'fname':f, 'size':os.path.getsize(fp)}
 similar_files = {}
 fp = file_map
@@ -19,15 +19,15 @@ for f in fp:
 		similar_files[fp[f]['fname']+str(fp[f]['size'])] = []
 	similar_files[fp[f]['fname']+str(fp[f]['size'])].append(f)
 for f in similar_files:
-	flog.write(f+'\n')
-	print f
-	flog.write('-'*len(f)+'\n')
-	print '-'*len(f)
 	if len(similar_files[f])>1:
+		flog.write(f+'\n')
+		print f
+		flog.write('-'*len(f)+'\n')
+		print '-'*len(f)
 		for fi in similar_files[f]:
 			flog.write(fi+'\n')
 			print fi
+		flog.write('\n')
 	# print
-	flog.write('\n')
 flog.close()
 
